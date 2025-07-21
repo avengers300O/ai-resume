30dcb68308777b1079d99f4785ab3ef22e386823
shivam <somujangid12@gmail.com>
2025-07-21 20:20:15 +0530

    Initial commit of existing project

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import { resumes } from "constant";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Ahead resume anaylizer" },
+    { name: "description", content: "ai resume anaylizer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center bg-no-repeat">
+      <Navbar></Navbar>
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track You Application & Resume Rating</h1>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        ) }
+      </section>
+    </main>
+  );
 }
